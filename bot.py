9e962cb17deddf8da748684835af1ca1c365b37b
pmc <werdse13@gmail.com>
2026-02-28 08:53:10 +0000

    Initial commit

--- bot.py
+++ bot.py
@@ -2,8 +2,9 @@ import discord
 from discord.ext import commands
 from scraper import seats_counter
 import asyncio
+import os
 
-TOKEN = "INSERT BOT TOKEN"
+TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
